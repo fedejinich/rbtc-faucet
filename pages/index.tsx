a461adff6b85b3be20914a95936c66928eb750fa
Fede Jinich <federico@iovlabs.org>
2019-11-14 15:48:45 -0300

    :recycle: preparing for responsiveness

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,18 +4,15 @@ import axios from 'axios';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Swal, { SweetAlertOptions } from 'sweetalert2';
-import { DispenseResponse } from '../types/types.d';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import '../assets/styles/index.css';
-import { apiUrl, newCaptchaUrl } from '../utils/env-util';
-import RskCard from '../components/rsk-card';
 import ReactCardCarousel from 'react-card-carousel';
-import Faucet from '../components/faucet';
 import RskLinkCard from '../components/rsk-link-card';
+import Faucet, { FaucetProps } from '../components/faucet';
+import Swal, { SweetAlertOptions } from 'sweetalert2';
+import { apiUrl, newCaptchaUrl } from '../utils/env-util';
+import { DispenseResponse } from '../types/types.d';
+import '../assets/styles/index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import '../assets/styles/globals.css';
-
-const RSK_TESTNET_CHAIN = 31;
 
 function App() {
   //Hooks
@@ -30,6 +27,23 @@ function App() {
 
   //Handles
   const handleFaucetButtonClick = async () => {
+    const swalSetup = (data: DispenseResponse): SweetAlertOptions => {
+      return {
+        titleText: data.titleText,
+        text: data.text,
+        type: data.type,
+        onClose: () => {
+          if (data.dispenseComplete) {
+            fetchCaptcha();
+            setDispenseAddress('');
+            setCaptchaValue('');
+          } else if (data.resetCaptcha) {
+            fetchCaptcha();
+          }
+        }
+      };
+    };
+    
     setLoading(true);
     axios
       .post(apiUrl() + '/dispense', {
@@ -63,27 +77,18 @@ function App() {
 
   //Methods
   const fetchCaptcha = async () => {
-    const result = await axios.post(
-      newCaptchaUrl(),
-    );
+    const result = await axios.post(newCaptchaUrl());
     setCaptcha(result.data);
   };
 
-  const swalSetup = (data: DispenseResponse): SweetAlertOptions => {
-    return {
-      titleText: data.titleText,
-      text: data.text,
-      type: data.type,
-      onClose: () => {
-        if (data.dispenseComplete) {
-          fetchCaptcha();
-          setDispenseAddress('');
-          setCaptchaValue('');
-        } else if (data.resetCaptcha) {
-          fetchCaptcha();
-        }
-      }
-    };
+  const faucetPropsDesktop: FaucetProps = {
+    captcha: captcha,
+    loading: loading,
+    dispenseAddress: dispenseAddress,
+    captchaValue: captchaValue,
+    onAddressChange: handleDispenseAddressChange,
+    onCaptchaValueChange: handleCaptchaValueChange,
+    onDispenseClick: handleFaucetButtonClick
   };
 
   return (
@@ -94,57 +99,59 @@ function App() {
         </Navbar.Brand>
       </Navbar>
       <div className="app-body vertical-center">
-        <Container className="m-0 p-0 w-100" style={{ maxWidth: '80%' }}>
-          <Row className="w-100" style={{ width: '100%' }}>
-            <Col>
-              <Faucet
-                captcha={captcha}
-                loading={loading}
-                dispenseAddress={dispenseAddress}
-                captchaValue={captchaValue}
-                onAddressChange={handleDispenseAddressChange}
-                onCaptchaValueChange={handleCaptchaValueChange}
-                onDispenseClick={handleFaucetButtonClick}
-              />
-            </Col>
-            <Col>
-              <Container className="h-100 w-100">
-                <ReactCardCarousel autoplay={true} autoplay_speed={5000}>
-                  <Row>
-                    <RskLinkCard
-                      link="https://developers.rsk.co/"
-                      icon={<img className="devportal-icon" />}
-                      title="DevPortal"
-                      backgroundColor="#00b41f"
-                      description="For developers by developers"
-                    />
-                  </Row>
-                  <Row>
-                    <RskLinkCard
-                      link="https://developers.rsk.co/develop/apps/wallets/"
-                      icon={<img className="wallet-icon" />}
-                      title="Wallets"
-                      backgroundColor="black"
-                      description="Try RSK with these wallets"
-                    />
-                  </Row>
-                  <Row>
-                    <RskLinkCard
-                      link="https://developers.rsk.co/tutorials/"
-                      icon={<img className="tutorials-icon" />}
-                      title="Tutorials"
-                      backgroundColor="#f26122"
-                      description="How to develop on RSK"
-                    />
-                  </Row>
-                </ReactCardCarousel>
-              </Container>
-            </Col>
-          </Row>
+        <Container className="m-0 p-0 w-100 container-rsk">
+          <DesktopCards faucetProps={faucetPropsDesktop} />
         </Container>
       </div>
     </div>
   );
 }
+
+interface DynamicCardsProps {
+  faucetProps: FaucetProps;
+}
+
+const DesktopCards = (props: DynamicCardsProps) => {
+  return (
+    <Row className="w-100">
+      <Col>
+        <Faucet {...props.faucetProps} />
+      </Col>
+      <Col>
+        <Container className="h-100 w-100">
+          <ReactCardCarousel autoplay={true} autoplay_speed={5000}>
+            <Row>
+              <RskLinkCard
+                link="https://developers.rsk.co/"
+                icon={<img className="devportal-icon" />}
+                title="DevPortal"
+                backgroundColor="#00b41f"
+                description="For developers by developers"
+              />
+            </Row>
+            <Row>
+              <RskLinkCard
+                link="https://developers.rsk.co/develop/apps/wallets/"
+                icon={<img className="wallet-icon" />}
+                title="Wallets"
+                backgroundColor="black"
+                description="Try RSK with these wallets"
+              />
+            </Row>
+            <Row>
+              <RskLinkCard
+                link="https://developers.rsk.co/tutorials/"
+                icon={<img className="tutorials-icon" />}
+                title="Tutorials"
+                backgroundColor="#f26122"
+                description="How to develop on RSK"
+              />
+            </Row>
+          </ReactCardCarousel>
+        </Container>
+      </Col>
+    </Row>
+  );
+};
 
 export default App;
